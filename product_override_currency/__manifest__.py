cae4c2344d971e3e78c982061c4d95337c5dbb43
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-11-30 13:38:38 +0100

    Added module product_override_currency.

---
+++ product_override_currency/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Product override currency",
+
+    'summary': """ Allow modify currency in products """,
+
+    'description': """ Allow modify currency in products """,
+
+    'author': "Eduwebgroup",
+
+    'website': "http://www.eduwebgroup.com",
+
+    'category': 'Sales Management',
+    'version': '1.0',
+
+    'depends': ['product'],
+
+    'data': [
+        'views/product_template.xml',
+    ],
+
+}
