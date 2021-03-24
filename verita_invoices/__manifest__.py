fb2aa56940f62f2d44428a200e717e1fb71d6767
Matt Pedrosa <35915547+MattPedrosa@users.noreply.github.com>
2021-03-25 04:02:00 +0800

    Add files via upload

---
+++ verita_invoices/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Verita Payment Import",
+
+    'summary': """ Import XLS/CSV File to Payment """,
+
+    'description': """
+        Import XLS/CSV File to Payment.
+    """,
+
+    'author': "EduwebGroup",
+    'website': "http://www.eduwebgroup.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.3',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base', 
+        'account',
+        'sale',
+        'product',
+        'account_reports',
+        'barcodes',
+        'school_finance'
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'data/automated_actions.xml',
+        'data/mail_template_data.xml',
+        'data/base_automation_data.xml',
+        'views/reports.xml',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/account_move_views.xml',
+        'views/account_move_templates.xml',
+        'views/account_payment_import_views.xml',
+        'wizards/account_payment_import_wizard_views.xml',
+        'reports/account_move_report_templates.xml',
+        'reports/account_move_reports.xml',
+    ]
+}
---
+++ verita_invoices/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import account_move
+from . import res_partner
+from . import sale_order
+from . import res_company
+from . import res_currency
+from . import account_journal
+from . import account_payment_import
+from . import journal_late_fee_range
