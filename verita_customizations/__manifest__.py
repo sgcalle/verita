0a9e546d7b5e260673cbcf68e59738eddc5eb7e9
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2021-04-20 19:45:24 +0200

    [IMP] verita_customization: Add custom page in reenrollment form

--- verita_customizations/__manifest__.py
+++ verita_customizations/__manifest__.py
@@ -12,8 +12,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.21',
+    'category': 'Customizations',
+    'version': '0.4',
 
     # any module necessary for this one to work correctly
     'depends': [
